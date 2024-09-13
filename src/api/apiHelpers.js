b2c516767c5697c30a12baf79c3a4f24829ebe9d
aboombadev <abhayshuklayt@gmail.com>
2024-09-13 15:50:22 -0700

    fetchAllContributions works

--- src/api/apiHelpers.js
+++ src/api/apiHelpers.js
@@ -1,68 +1,71 @@
-import axios from "axios";
 import { config } from "dotenv";
+
+//TODO: make api find out how many org im in and account for that
+//TODO: get profile image from git, set that as main image
 
 // Load API key
 config();
 const apiKey = process.env.GH_API_ACCESS_TOKEN;
 
-// Call/Compile data
+async function graphqlQuery(query, variables = {}) {
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  const data = await response.json();
+  return data;
+}
+
+// Read Github data & write to JSON
 export default async function compileAndWriteGHData() {
   // const { followers, diskUsage } = await getUserInfo();
   // const diskUsageMB = (diskUsage / 1000).toFixed(2);
 
-  const totalContributions = await getTotalContributions();
+  const totalContributions = await computeTotalContributions();
   console.log("total", totalContributions);
 }
 
-async function graphqlQuery(query) {
-  try {
-    const response = await axios.post(
-      `https://api.github.com/graphql`,
-      { query },
-      {
-        headers: { Authorization: `Bearer ${apiKey}` },
-      },
-    );
+async function computeTotalContributions() {
+  const contributions = await fetchAllContributions();
+  const issues = await fetchAllIssues();
+  const prs = await fetchAllPullRequests();
+  const reviews = await fetchAllReviews();
 
-    return response.data.data;
-  } catch (error) {
-    console.error(
-      `Request failed with status code ${error.response ? error.response.status : "unknown"}`,
-    );
-    throw error;
+  const extraContributions = 2; //Account creation, orgs joined
+
+  return (
+    contributions + len(issues) + len(prs) + len(reviews) + extraContributions
+  );
+}
+
+async function fetchAllContributions() {
+  let total = 0;
+  const startYear = 2022;
+  const currentYear = new Date().getFullYear();
+
+  for (let year = startYear; year <= currentYear; year++) {
+    console.log(year, await getYearlyContributions(year));
+    total += await getYearlyContributions(year);
   }
+
+  // total += 2; //Account creation, organizations joined
+
+  return total;
 }
 
-async function getTotalContributions() {
-  const contributionYears = await getContributionYears();
-
-  const yearlyContrib = await getYearlyContributions(contributionYears[0]);
-  console.log(yearlyContrib);
-
-  return totalContributions;
-}
-
-async function getContributionYears() {
-  const query = `
-  query {
-    viewer {
-      contributionsCollection {
-        contributionYears
-      }
-    }
-  }
-`;
-  const rawData = await graphqlQuery(query);
-  const { contributionYears } = rawData.viewer.contributionsCollection;
-
-  return contributionYears;
-}
-
+// Fetch yearly contribtuions
 async function getYearlyContributions(year) {
-  const query = `
-    query {
+  const commitsQuery = `
+    query($from: DateTime!, $to: DateTime!) {
       viewer {
-        contributionsCollection {
+        contributionsCollection(from: $from, to: $to) {
           contributionCalendar {
             totalContributions
           }
@@ -71,12 +74,143 @@ async function getYearlyContributions(year) {
     }
   `;
 
-  const rawData = await graphqlQuery(query);
-  console.log(rawData);
+  const fromDate = `${year}-01-01T00:00:00Z`;
+  const toDate = `${year + 1}-01-01T00:00:00Z`;
 
-  const { contributionCalendar } = rawData.viewer.contributionsCollection;
+  try {
+    const rawCommits = await graphqlQuery(commitsQuery, {
+      from: fromDate,
+      to: toDate,
+    });
 
-  return contributionCalendar;
+    const totalCommits =
+      rawCommits.data.viewer.contributionsCollection.contributionCalendar
+        .totalContributions;
+
+    return totalCommits;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return 0;
+  }
+}
+
+// Fetch paginated information
+async function fetchAllIssues() {
+  const issuesQuery = `
+  query($afterCursor: String) {
+  viewer {
+    issues(first: 100, after: $afterCursor, orderBy: { field: CREATED_AT, direction: DESC }) {
+      edges {
+        node {
+          createdAt
+          title
+          body
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+  }
+`;
+
+  let issues = [];
+  let hasNextPage = true;
+  let endCursor = null;
+
+  while (hasNextPage) {
+    const result = await graphqlQuery(issuesQuery, { afterCursor: endCursor });
+    issues = issues.concat(result.viewer.issues.edges.map((edge) => edge.node));
+    hasNextPage = result.viewer.issues.pageInfo.hasNextPage;
+    endCursor = result.viewer.issues.pageInfo.endCursor;
+  }
+
+  return issues;
+}
+
+async function fetchAllPullRequests() {
+  const prsQuery = `
+  query($afterCursor: String) {
+  viewer {
+    pullRequests(first: 100, after: $afterCursor, orderBy: { field: CREATED_AT, direction: DESC }) {
+      edges {
+        node {
+          createdAt
+          title
+          body
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+  }
+  `;
+
+  let prs = [];
+  let hasNextPage = true;
+  let endCursor = null;
+
+  while (hasNextPage) {
+    const result = await graphqlQuery(prsQuery, { afterCursor: endCursor });
+    prs = prs.concat(result.viewer.pullRequests.edges.map((edge) => edge.node));
+    hasNextPage = result.viewer.pullRequests.pageInfo.hasNextPage;
+    endCursor = result.viewer.pullRequests.pageInfo.endCursor;
+  }
+
+  return prs;
+}
+
+async function fetchAllReviews() {
+  const reviewsQuery = `
+  query($afterCursor: String) {
+  viewer {
+    pullRequests(first: 100, after: $afterCursor, orderBy: { field: CREATED_AT, direction: DESC }) {
+      edges {
+        node {
+          reviews(first: 100) {
+            edges {
+              node {
+                createdAt
+                body
+                author {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+  }
+  `;
+
+  let reviews = [];
+  let hasNextPage = true;
+  let endCursor = null;
+
+  while (hasNextPage) {
+    const result = await graphqlQuery(reviewsQuery, { afterCursor: endCursor });
+    const prs = result.viewer.pullRequests.edges;
+
+    for (const pr of prs) {
+      reviews = reviews.concat(pr.node.reviews.edges.map((edge) => edge.node));
+    }
+
+    hasNextPage = result.viewer.pullRequests.pageInfo.hasNextPage;
+    endCursor = result.viewer.pullRequests.pageInfo.endCursor;
+  }
+
+  return reviews;
 }
 
 // async function getUserInfo() {
@@ -85,34 +219,6 @@ async function getYearlyContributions(year) {
 //       headers: { Authorization: `token ${apiKey}` },
 //     })
 //     .then((response) => {
-//       return {
-//         followers: response.data.followers,
-//         diskUsage: response.data.disk_usage,
-//       };
-//     })
-//     .catch((error) => {
-//       console.error(
-//         `Request failed with status code ${error.response ? error.response.status : "unknown"}`
-//       );
-//     });
-// }
-
-// async function getLinesModified() {
-//   const getPropOverAllRepos = (repos) => {
-//     let linesModified = 0;
-
-//     for (const repo of repos) {
-//       console.log(`${repo}`);
-//     }
-//   };
-
-//   return axios
-//     .get("https://api.github.com/user/repos", {
-//       headers: { Authorization: `token ${apiKey}` },
-//     })
-//     .then((response) => {
-//       // Get total commits
-//       getPropOverAllRepos(response.data);
 //       return {
 //         followers: response.data.followers,
 //         diskUsage: response.data.disk_usage,
