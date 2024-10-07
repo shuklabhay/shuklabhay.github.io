2058382bb1ab0ed59c20edabfb5655ed2b3c6dba
Abhay Shukla <105139789+shuklabhay@users.noreply.github.com>
2024-10-07 13:53:43 -0700

    fix routing (#46)

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
-import path from "path";
-
-function copyDirectory(src: string, dest: string) {
-  if (!fs.existsSync(dest)) {
-    fs.mkdirSync(dest, { recursive: true });
-  }
-
-  const entries = fs.readdirSync(src, { withFileTypes: true });
-
-  for (const entry of entries) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
-
-    if (entry.isDirectory()) {
-      copyDirectory(srcPath, destPath);
-    } else {
-      fs.copyFileSync(srcPath, destPath);
-    }
-  }
-}
 
 export default defineConfig({
   plugins: [
@@ -29,11 +9,6 @@ export default defineConfig({
       name: "copy-static-deps",
       closeBundle: () => {
         fs.copyFileSync("404.html", "dist/404.html");
-
-        const srcDir = path.resolve(__dirname, "src/static");
-        const destDir = path.resolve(__dirname, "dist/static");
-
-        copyDirectory(srcDir, destDir);
       },
     },
   ],
