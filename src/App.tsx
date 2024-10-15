bbeaa5915fdfea87fe09c4bc1a60855d3c0ec148
Abhay Shukla <105139789+shuklabhay@users.noreply.github.com>
2024-10-14 22:57:55 -0700

    remove google analytics (#76)

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,16 @@ import "@mantine/carousel/styles.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { theme } from "./utils/theme.ts";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InteractiveSite from "./pages/InteractiveSite.tsx";
 import { AppProvider } from "./utils/appContext.tsx";
 import PlainTextSite from "./pages/PlainTextSite.tsx";
-import { useEffect } from "react";
-import ReactGA from "react-ga4";
-
-const AnalyticsTrackingID = import.meta.env.VITE_TRACKING_ID;
-
-function RouteTracker() {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize(AnalyticsTrackingID);
-  }, [AnalyticsTrackingID]);
-
-  useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname,
-      title: location.pathname,
-    });
-  }, [location]);
-
-  return null;
-}
 
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <AppProvider>
         <BrowserRouter>
-          <RouteTracker />
           <Routes>
             <Route path="/" element={<InteractiveSite />} />
             <Route path="/plaintext" element={<PlainTextSite />} />
