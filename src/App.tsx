312b24cf4bef19826805749017c4b8322e10011a
aboombadev <abhayshuklayt@gmail.com>
2024-10-17 01:33:23 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InteractiveSite from "./pages/InteractiveSite.tsx";
 import { AppProvider } from "./utils/appContext.tsx";
 import PlainTextSite from "./pages/PlainTextSite.tsx";
-import { HelmetProvider } from "react-helmet-async";
 
 export default function App() {
   return (
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <AppProvider>
-        <HelmetProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<InteractiveSite />} />
-              <Route path="/plaintext" element={<PlainTextSite />} />
-            </Routes>
-          </BrowserRouter>
-        </HelmetProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<InteractiveSite />} />
+            <Route path="/plaintext" element={<PlainTextSite />} />
+          </Routes>
+        </BrowserRouter>
       </AppProvider>
     </MantineProvider>
   );
