7678d46ff9d8e5543de9b35228da44d5137d4286
aboombadev <abhayshuklayt@gmail.com>
2025-09-17 14:46:23 -0700

    config stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "@mantine/carousel/styles.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { theme } from "./utils/theme.ts";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import InteractiveSite from "./pages/InteractiveSite.tsx";
-import { AppProvider } from "./utils/appContext.tsx";
 import PlainTextSite from "./pages/PlainTextSite.tsx";
+import { AppProvider } from "./utils/appContext.tsx";
 
 export default function App() {
   return (
