7fc34f0a80384e976bc11036c2774a857fd9bcea
Abhay Shukla <abhayshuklayt@gmail.com>
2026-05-16 22:26:35 -0700

    Keep post back link in current tab

--- src/components/PostBackLink.tsx
+++ src/components/PostBackLink.tsx
@@ -29,8 +29,6 @@ export default function PostBackLink(): JSX.Element {
         to={backTargetPath}
         viewTransition={!prefersReducedMotion}
         state={{ fromPost: true }}
-        target="_blank"
-        rel="noopener noreferrer"
         style={{
           color: "white",
           textDecoration: "none",
